181de2dece8ab3bf20fe63c53156c3a59ba13d95
Jonathan <jnt.rodrigues68@gmail.com>
2018-08-13 09:45:00 +0200

    hamburger menu

---
+++ menu.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jonathan Rodrigues on 10/08/2018.
+*/
+$(document).ready(function(){
+    /* sidebar */
+    $(".hamburger").on("touchstart click", function(e) {
+        /* animation cross */
+        $(this).toggleClass("is-active");
+        /* open menu */
+        e.preventDefault(), $(".sidebar").toggleClass("active"), $(".nav").removeClass("active"), $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
+    }), $(".sidebar .sidebar-overlay").on("touchstart click", function(e) {
+        e.preventDefault(), $(".hamburger").click(),$('.hamburger').removeClass("is-active"), $(this).removeClass("fadeIn").addClass("fadeOut")
+    })
+});
+
